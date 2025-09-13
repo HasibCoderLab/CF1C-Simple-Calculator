5eeb8fcba49508b8694e9a9029161c9a9ff88429
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-13 22:45:02 +0600

    solve 3rd  Problem with C++ in codeforces

--- Simple-Calculators.cpp
+++ Simple-Calculators.cpp
@@ -8,9 +8,13 @@
 // ================================================
 #include <iostream>
 using namespace std;
-int main(){
- int X, Y;
- cout<<X<<Y;
- cin>>X>>Y;
+int main()
+{
+    int X, Y;
+    cin >> X >> Y;
+    cout << "X + Y = " << X + Y << endl;  
+    cout << "X * Y = " << X * Y << endl;
+    cout << "X - Y = " << X - Y << endl;
+
     return 0;
 }
