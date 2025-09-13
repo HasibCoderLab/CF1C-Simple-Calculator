2233d7a7e49478febe9fc04ebdba1545df2e3c7f
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-13 22:15:19 +0600

    Add Information

---
+++ Simple-Calculator.cpp
@@ -0,0 +1,16 @@
+// ================================================
+//                Platform : Codeforces
+//                Contest  : CF1C
+//                Problem  : Simple-Calculator
+//                Language : C++
+//                Topic    : [Simple-Calculator]
+//                File     : Simple-Calculators.cpp
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+ int X, Y;
+ cout<<X<<Y;
+ cin>>X>>Y;
+    return 0;
+}
